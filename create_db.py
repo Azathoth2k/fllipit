94f6a15706ff33c2e53069503db7abd2b9239a67
ryan.foley <ryan.foley@kepware.com>
2014-12-01 13:05:58 -0500

    Updated database creation script to include playoff data

--- create_db.py
+++ create_db.py
@@ -1,7 +1,10 @@
+from flask.ext.sqlalchemy import SQLAlchemy
 from models import DB, Team
+from fllipit import APP
 import api
 import random
 
+DB.app = APP
 DB.create_all()
 
 teams = []
@@ -15,10 +18,22 @@ for i in range(80):
         round3=random.randrange(0, 400, 1))
     teams.append(team)
 
-sortedTeams = api.rankTeams(teams)
+# Top 12
+for team in api.rankTeams(teams)[:12]:
+    team.advanceTo4 = True
+    team.round4=random.randrange(0, 400, 1)
 
-# TODO add playoff round scores to top 12/4/2    
+# Top 4
+for team in sorted(teams, key=lambda x: x.round4, reverse=True)[:4]:
+    team.advanceTo5 = True
+    team.round5=random.randrange(0, 400, 1)
+
+#Top 2
+for team in sorted(teams, key=lambda x: x.round5, reverse=True)[:2]:
+    team.advanceTo6 = True
+    team.round6=random.randrange(0, 400, 1)
     
+# Add all teams to database
 for team in teams:
     DB.session.add(team)
     
