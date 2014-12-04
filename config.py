72056b89cad440c4bd7fa35adf5faa6c180b22e0
rtfoley <rtfoley@gmail.com>
2014-12-04 18:36:57 -0500

    Turned debug mode off in config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'fllipit.db')
-DEBUG = True
+DEBUG = False
 EVENT_NAME = 'Maine FLL Championship'
 TEST_DB = False
 DB_FILE = "path\to\access\database.accdb"
