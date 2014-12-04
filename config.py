193c31e9dc25cefb47331775a9f61e66066b5efb
rtfoley <rtfoley@gmail.com>
2014-12-04 18:50:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'fllipit.db')
 DEBUG = False
 EVENT_NAME = 'Maine FLL Championship'
 TEST_DB = False
-DB_FILE = "path\to\access\database.accdb"
+DB_FILE = 'C:\\path\\to\\access\\database.accdb'
