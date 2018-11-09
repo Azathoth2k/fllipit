998822a44e11700cbe360c8ff238500d453ce3aa
Ryan Foley <rtfoley@gmail.com>
2018-11-08 21:14:50 -0500

    Disable sqlalchemy modification tracking

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'fllipit.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = False
 EVENT_NAME = 'Maine FLL Championship'
 TEST_DB = False
