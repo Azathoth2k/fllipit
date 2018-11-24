cd2474a4d3cdfab7fb857be66bc23fb8dd09a64e
azathoth2k <az2k@verizon.net>
2018-11-24 15:33:46 -0500

    Modified config file

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'fllipit.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = False
-EVENT_NAME = 'Maine FLL Championship'
+EVENT_NAME = 'Littleton FLL Qualifier'
 TEST_DB = False
-DB_FILE = 'C:\\path\\to\\access\\database.accdb'
+DB_FILE = 'E:\\users\\m172108\\github\\database\\FLL 2018 INTO ORBIT blank as of 100918.accdb'
 
 # The number of qualifying rounds, minimum is 
-USE_5_QUAL_ROUNDS = False
+USE_5_QUAL_ROUNDS = True
